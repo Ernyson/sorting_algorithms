f7290cef597d48348ce972dd33b46900fd982dd6
Awoyemi Boluwatife <awoyemiboluwatife@gmail.com>
2022-10-12 02:32:30 +0000

    shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,9 +1,9 @@
-#include "sort.h"
+i#include "sort.h"
 
 /**
  * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
+ * @a: The first integer to swap
+ * @b: The second integer to swap
  */
 
 void swap_ints(int *a, int *b)
