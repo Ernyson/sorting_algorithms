1b36ef74cd96d80b65e48620379b8ad9b0e5cb98
ernyson <ernestaiji1@gmail.com>
2022-10-11 18:42:42 -0700

    insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,53 +1,50 @@
 #include "sort.h"
 
 /**
- * swap_nodes - Swaps two nodes
- * @node1: Pointer to address of node 1
- * @node2: Pointer to address of node 2
- * @list: blbla
- * Return: Nothing
- */
-void swap_nodes(listint_t **list, listint_t **node1, listint_t **node2)
-{
-	listint_t *tmp = (*node1)->prev;
-
-	(*node1)->next = (*node2)->next;
-
-	if ((*node2)->next != NULL)
-		(*node2)->next->prev = *node1;
-
-	(*node1)->prev = *node2;
-	(*node2)->next = *node1;
-	(*node2)->prev = tmp;
-
-	if ((*node2)->prev != NULL)
-		(*node2)->prev->next = *node2;
-	else
-		*list = *node2;
-}
-
-/**
- * insertion_sort_list - Sorts a doubly linked list using Insertion Sort
- * @list: Pointer to head of the linked list
- * Return: Nothing
+ * insertion_sort_list - inserts right unsorted side into left sorted side
+ * @list: doubly linked list to sort
+ *
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *actual = NULL, *key = NULL;
+	listint_t *c, *p, *nextnode;
 
-	if (!list || !(*list))
+	if (list == NULL || !(*list) || (*list)->next == NULL)
 		return;
-
-	for (actual = *list; actual != NULL; actual = actual->next)
+	c = (*list)->next;
+	nextnode = c->next;
+	while (c)
 	{
-		while (actual->next != NULL && actual->n > actual->next->n)
+		if (c->n < c->prev->n)
 		{
-			key = actual->next;
-			while (key->prev != NULL && key->n < key->prev->n)
+			p = c->prev;
+			while (p && (c->n < p->n))
 			{
-				swap_nodes(list, &key->prev, &key);
+				if (!(p->prev))
+				{
+					p->prev = c;
+					c->prev->next = c->next;
+					if (c->next)
+						c->next->prev = c->prev;
+					c->next = p;
+					c->prev = NULL;
+					*list = c;
+				}
+				else
+				{
+					c->prev->next = c->next;
+					if (c->next)
+						c->next->prev = c->prev;
+					p->prev->next = c;
+					c->prev = p->prev;
+					p->prev = c;
+					c->next = p;
+				}
 				print_list(*list);
+				p = c->prev;
 			}
 		}
+		c = nextnode;
+		c ? (nextnode = c->next) : (nextnode = NULL);
 	}
 }
